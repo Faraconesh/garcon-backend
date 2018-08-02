9576e230944b826b5ad6373854995d8c2233d4e1
faraconesh <info@faraconesh.com>
2018-08-02 23:49:19 +0430

    Fix picture for food model, fix url for pictures

--- api/models.py
+++ api/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 class Food(models.Model):
     name = models.CharField(verbose_name=_('Food name'), max_length=100)
+    picture = models.FileField(blank=True, null=True)
     price = models.DecimalField(verbose_name=_('Price'), max_digits=10, decimal_places=3)
-    details = models.CharField(verbose_name=_('Details'), max_length=500)
-
+    details = models.CharField(verbose_name=_('Details'), max_length=500, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -23,7 +23,7 @@ class Order(models.Model):
     food = models.ManyToManyField(to=Food, related_name=_('Food'))
     submitDateTime = models.DateTimeField(verbose_name=_('Submit date time'), auto_now_add=True)
     orderDateTime  = models.DateTimeField(verbose_name=_('Order date time'), auto_now_add=True)
-    details = models.CharField(verbose_name=_('Details'),  max_length=500)
+    details = models.CharField(verbose_name=_('Details'),  max_length=500, null=True)
 
     # def __str__(self):
     #     return self.name
