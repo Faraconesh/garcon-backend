0530b30b1bb54743bbcf4bf7781ea2cc675adaa4
r3j3c73d <info@faraconesh.com>
2018-08-25 00:01:00 +0430

    some change in urls for reset pass (not completed)

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from api.views import RestaurantList, FoodList, FoodDetails, FoodCreation, OrderCreation, OrderList, UserOrderList, OrderDetails, UserOrderDetails
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('accounts/', include('rest_auth.urls')),
-    path('accounts/registration', include('rest_auth.registration.urls')),
+    path('accounts/registration/', include('rest_auth.registration.urls')),
+    re_path(r'password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        TemplateView.as_view(template_name="password_reset_confirm.html"),
+        name='password_reset_confirm'),
     path('restaurantList', RestaurantList.as_view(), name='Restaurant List'),
     path('foodList', FoodList.as_view(), name='Food List'),
     path('foodCreation', FoodCreation.as_view(), name='Food Creation'),
